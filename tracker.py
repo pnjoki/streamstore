7c711f6381d4db92a25e0f0372eb61263ed0e2f6
pnjoki <paulgitonga58@gmail.com>
2025-11-23 02:23:46 +0300

    updates

---
+++ tracker.py
@@ -0,0 +1,26 @@
+from confluent_kafka import Consumer
+import json
+
+Consumer_config = {
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'tracker_group',
+    'auto.offset.reset': 'earliest'
+}
+
+consumer = Consumer(Consumer_config)
+consumer.subscribe(['orders'])
+try:
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            print(f"Consumer error: {msg.error()}")
+            continue
+
+        order = json.loads(msg.value().decode('utf-8'))
+        print(f"Received order: {order}") 
+except KeyboardInterrupt:
+    pass
+finally:
+    consumer.close()
